8b0653f0191f6bc1c2a886307f139a39cb009466
MANISH PRABHAKAR <manishprabhakar@outlook.com>
2022-12-25 07:34:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.viewmodel
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+
+class MainActivity : AppCompatActivity()
+{
+    lateinit var txtcounter:TextView
+    lateinit var mainViewModel: MainViewModel
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        txtcounter = findViewById(R.id.counter)
+        setText()
+    }
+
+    private fun setText() {
+       txtcounter.text = mainViewModel.count.toString()
+    }
+    fun increment(v:View)
+    {
+        mainViewModel.increment()
+        setText()
+    }
+
+
+}
